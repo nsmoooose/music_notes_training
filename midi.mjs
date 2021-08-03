7e2c49b8462c5475d867fba20dad9680e17120f8
Henrik Nilsson <nsmoooose@gmail.com>
2021-08-03 20:00:24 +0200

    Convert space to tabs

--- midi.mjs
+++ midi.mjs
@@ -1,41 +1,41 @@
 export const MidiConstants = {
-    CHANNEL1_NOTE_OFF: 128,
-    CHANNEL1_NOTE_ON: 144
+	CHANNEL1_NOTE_OFF: 128,
+	CHANNEL1_NOTE_ON: 144
 };
 
 
 function generate_midi_notes() {
-    let answers = [
-        "C",
-        ["C♯", "D♭"],
-        "D",
-        ["D♯", "E♭"],
-        "E",
-        "F",
-        ["F♯", "G♭"],
-        "G",
-        ["G♯", "A♭"],
-        "A",
-        ["A♯", "B♭"],
-        "B"
-    ];
+	let answers = [
+		"C",
+		["C♯", "D♭"],
+		"D",
+		["D♯", "E♭"],
+		"E",
+		"F",
+		["F♯", "G♭"],
+		"G",
+		["G♯", "A♭"],
+		"A",
+		["A♯", "B♭"],
+		"B"
+	];
 
-    let result = {};
+	let result = {};
 
-    /* A0 = 21 */
-    const start_offset = 21 - answers.indexOf("A");
-    for(let i = 21; i <= 108; i++ ) {
-        let octave = Math.floor((i - start_offset) / answers.length);
-        let note = (i - start_offset) % answers.length;
+	/* A0 = 21 */
+	const start_offset = 21 - answers.indexOf("A");
+	for(let i = 21; i <= 108; i++ ) {
+		let octave = Math.floor((i - start_offset) / answers.length);
+		let note = (i - start_offset) % answers.length;
 
-        let x = answers[note];
-        if(Array.isArray(answers[note])) {
-            result[i] = answers[note].map(x => x[0] + octave + x[1]);
-        } else {
-            result[i] = answers[note] + octave;
-        }
-    }
-    return result;
+		let x = answers[note];
+		if(Array.isArray(answers[note])) {
+			result[i] = answers[note].map(x => x[0] + octave + x[1]);
+		} else {
+			result[i] = answers[note] + octave;
+		}
+	}
+	return result;
 }
 
 export const MidiPianoNotes = generate_midi_notes();
